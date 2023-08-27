2ba6230baf69cc28f26ec4ccf6b616643507ae23
debelopguy <zyravlev155@gmail.com>
2023-08-28 01:31:14 +0300

    v1.1

--- BIOME_HOPPER3001.py
+++ BIOME_HOPPER3001.py
@@ -1,6 +1,6 @@
 #BIOME HOPPER 3000!!!!!
 
-import requests,webbrowser,json,pyautogui,time,datetime,subprocess,os,keyboard
+import discord_webhook,webbrowser,json,pyautogui,time,datetime,subprocess,os,keyboard
 from PIL import Image
 
 settings = None
@@ -8,7 +8,7 @@ with open("./settings.json","r",encoding="utf-8") as file:
     settings=json.load(file)
     print("yooooooo loaded! beginning in 2 seconds.")
     time.sleep(2)
-#webhook = Webhook.from_url(settings["webhook"])
+
 # send data:
 def returnctime():
     return datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
@@ -16,8 +16,11 @@ def sendbywebhook(the):
     if settings["webhook"]=="None":
         return
     the["content"]=the["content"]+" ("+str(returnctime())+")"
-    requests.post(settings["webhook"],the)
-    #await webhook.send(content=the["content"],file=the["file"],files=the["files"], tts=the["tts"], embed=the["embed"], embeds=the["embeds"], delete_after=the["delete_after"], suppress_embeds=the["suppress_embeds"], silent=the["silent"])
+    wh = discord_webhook.DiscordWebhook(url=settings["webhook"],content=the["content"])
+    if ('file' in the):
+        with open(the["file"],"rb") as f:
+            wh.add_file(file=f.read(),filename=the["file"])
+    wh.execute()
 data = {
     "content":"# SUPER BIOME HOPPER 😎😎😎 3000 STARTED"}
 sendbywebhook(data)
@@ -32,11 +35,12 @@ def mathclamp(num,min,max):
 def findimage(imagename,conf):
     return pyautogui.locateOnScreen("./images/"+imagename,confidence=conf)
 skippablebosses={"paladin","fallen","machine","archmage","controller","steve","ripper"}
-worthskipping = {#793,81 ; 795,328
-"blizzard","flame","forest","irradiated","moon","pvc","star","storm","DIDNT_LOAD"
-}
-rarebiomes = {"hvuh","void","angel","cult","bene","asta"}
+worthskipping = settings["skippablebiomes"]
+rarebiomes = settings["rarebiomes"]
+finishall = False
 def finishfunc(key):
+    global finishall
+    finishall=True
     data = {
         "content":"## PRESSED STOP KEY ("+(settings["keytopause"]).upper()+") STOPPING NOW",
     }
@@ -45,40 +49,55 @@ def finishfunc(key):
 
 keyboard.on_press_key(settings["keytopause"],finishfunc)
 def unbreakablehumanspirit():
+    global finishall
     timestried=0
     biomefound = False
     whichbiomefound = "None"
     while biomefound==False:
+        if finishall==True:
+            break
         loadedin = False
+        loadedtimeout = 0
         whichbiomefound="None"
         data = {
         "content":"* opening the vip link now",
         }
         sendbywebhook(data)
-        webbrowser.open_new_tab(settings["vip"])
-        while loadedin==False:
-            #craftingmenu.png
+        if finishall==False:
+            webbrowser.open_new_tab(settings["vip"])
+        while loadedin==False and loadedtimeout<180:
             time.sleep(1)
+            loadedtimeout+=1
             if findimage("craftingmenu.png",.7)!=None:
                 loadedin = True
         #close browser here so it doesnt eat your ram 🐏
         subprocess.call("TASKKILL /IM "+settings["browsertoclose"]+".exe /F",shell= True)
+        if loadedin==False:
+            #timed out, wait a little then restart
+            if finishall==False:
+                subprocess.call("TASKKILL /IM RobloxPlayerBeta.exe /F",shell= True)
+            time.sleep(5)
+            data = {
+            "content":"## ⚠ Roblox took too long to load! <@"+settings["userid"]+"> Restarting.",
+            }
+            sendbywebhook(data)
+            return
         screenshot = pyautogui.screenshot()
         screenshot.save("tempscreen.png")
         towait = mathclamp(settings["waitbeforerejoin"],20,120)
-        #with open("tempscreen.png","rb") as fil:
-            #scr = discord.file(fil)
         data = {
-        "content":"## Roblox loaded! Resetting to fix camera.",
-        #"file": scr
+        "content":"## Roblox loaded!"+(settings["resetuponload"]==True and "Resetting to fix camera." or ""),
+        "file":"tempscreen.png",
         }
         sendbywebhook(data)
-        os.remove("tempscreen.png")
-        keyboard.press_and_release("esc")
-        time.sleep(.1)
-        keyboard.press_and_release("r")
-        time.sleep(.1)
-        keyboard.press_and_release("enter")
+        #os.remove("tempscreen.png") gets overwritten anyways, shouldnt take up that much space
+        #this is really inconsistent i should find a better way of checking if ur camera is the wrong way around
+        if settings["resetuponload"]==True:
+            keyboard.press_and_release("esc")
+            time.sleep(.1)
+            keyboard.press_and_release("r")
+            time.sleep(.1)
+            keyboard.press_and_release("enter")
         #click once to focus in the window and then zoomout
         data = {
         "content":"* focusing into roblox.",
@@ -101,20 +120,28 @@ def unbreakablehumanspirit():
         "content":"* Waiting "+str(towait)+" seconds now.",
         }
         sendbywebhook(data)
-        keyboard.press(mathclamp(settings["whichslottoequip"]+1,2,10))#equip item
         for i in range(1,towait,1):
             time.sleep(1)
+            if finishall==True:
+                break
             currentscreen = pyautogui.screenshot()
+            keyboard.press_and_release(mathclamp(settings["whichslottoequip"]+1,2,10))#equip item
             pyautogui.click(button="left",x=1600/2,y=900/2)
             keyboard.press_and_release('z, x, c')#incase its an item
-            #search for biome chat message
+            if i%15==0:#reset every 15s for alignment
+                keyboard.press_and_release("esc")
+                time.sleep(.5)
+                keyboard.press_and_release("r")
+                time.sleep(.5)
+                keyboard.press_and_release("enter")
+            #search for biome pixels
             yepifoundit = False
             nahskip = False
             #check rare first
             for biome in rarebiomes:
                 #check 2 pixels #793,81 ; 795,328
                 biomescreenshot = Image.open("./images/"+biome+".png")
-                if currentscreen.getpixel((793,81))==biomescreenshot.getpixel((793,81)) and currentscreen.getpixel((793,328))==biomescreenshot.getpixel((793,328)):
+                if currentscreen.getpixel((793,81))==biomescreenshot.getpixel((793,81)) and currentscreen.getpixel((1222, 345))==biomescreenshot.getpixel((1222, 345)):
                     yepifoundit=True
                     whichbiomefound=biome
                     break
@@ -127,7 +154,8 @@ def unbreakablehumanspirit():
                         break
                 for bad in worthskipping:
                     biomescreenshot = Image.open("./images/"+bad+".png")
-                    if currentscreen.getpixel((793,81))==biomescreenshot.getpixel((793,81)) and currentscreen.getpixel((793,328))==biomescreenshot.getpixel((793,328)):
+                    #1222, 345
+                    if currentscreen.getpixel((793,81))==biomescreenshot.getpixel((793,81)) and currentscreen.getpixel((1222, 345))==biomescreenshot.getpixel((1222, 345)):
                         nahskip=True
                         whichbiomefound=bad
                         break
@@ -135,6 +163,9 @@ def unbreakablehumanspirit():
             data = {
             "content":"* "+str(i)+"/"+str(towait)+"s passed; good biome: "+str(yepifoundit)+", bad biome: "+str(nahskip)+", which: "+str(whichbiomefound),
             }
+            if i%settings["screenshotstatusinterval"]==0:
+                currentscreen.save("tempscreen.png")
+                data["file"]="tempscreen.png"
             sendbywebhook(data)
             if yepifoundit==True:
                 biomefound=True
@@ -143,17 +174,62 @@ def unbreakablehumanspirit():
                 break
         if biomefound==True:
             data = {
-            "content":"# "+(whichbiomefound).upper()+" FOUND @everyone\n# "+settings["vip"]+"\nonly took "+str(timestried)+" tries!"}
+            "content":"# "+(whichbiomefound).upper()+" FOUND "+(settings["pingeveryoneuponrare"]==True and "@everyone" or "<@"+settings["userid"])+">"+"\n# "+settings["vip"]+"\nonly took "+str(timestried)+" tries!"+(settings["afkfarm"]==True and "\n### AFK FARM IS ON, WILL CONTINUE RUNNING" or "")}
             sendbywebhook(data)
+            if settings["afkfarm"]==True:
+                #here we go
+                biomefound=False #so that it doesnt stop after this is done
+                rarebiomeended=False
+                afktimeelapsed=0
+                #start afk loop here
+                while rarebiomeended==False:
+                    if finishall==True:
+                        break
+                    time.sleep(1)
+                    afktimeelapsed+=1
+                    keyboard.press_and_release(mathclamp(settings["slottoafkfarm"]+1,2,10))#equip item
+                    pyautogui.click(button="left",x=1600/2,y=900/2)
+                    keyboard.press_and_release('z, x, c')#incase its an item
+                    if afktimeelapsed%30==0:
+                        keyboard.press_and_release("esc")
+                        time.sleep(.5)
+                        keyboard.press_and_release("r")
+                        time.sleep(.5)
+                        keyboard.press_and_release("enter")
+
+                    currentscreen = pyautogui.screenshot()
+                    biomescreenshot = Image.open("./images/normal.png")
+                    if currentscreen.getpixel((793,81))==biomescreenshot.getpixel((793,81)) and currentscreen.getpixel((1222, 345))==biomescreenshot.getpixel((1222, 345)):
+                        rarebiomeended=True
+
+                    if rarebiomeended==True:
+                        screenshot = pyautogui.screenshot()
+                        screenshot.save("tempscreen.png")
+                        timestried=0
+                        data = {
+                        "content":"## "+(whichbiomefound).upper()+" ended in "+str(afktimeelapsed)+"s!\nmacro will continue running as normal.",
+                        "file":"tempscreen.png"}
+                        sendbywebhook(data)
+
+                        if finishall==False:
+                            subprocess.call("TASKKILL /IM RobloxPlayerBeta.exe /F",shell= True)
+                    else:
+                        data = {
+                        "content":"* "+(whichbiomefound).upper()+" has been going for "+str(afktimeelapsed)+"s."}
+                        if afktimeelapsed%settings["screenshotstatusinterval"]==0:
+                            currentscreen.save("tempscreen.png")
+                            data["file"]="tempscreen.png"
+                        sendbywebhook(data)
         else:
             timestried+=1
             data = {
             "content":"* skipped "+whichbiomefound+". ["+settings["strugglemessage"]+"]("+settings["struggleattachment"]+") #"+str(timestried)}
             sendbywebhook(data)
             #close roblox here
-            subprocess.call("TASKKILL /IM RobloxPlayerBeta.exe /F",shell= True)
+            if finishall==False:
+                subprocess.call("TASKKILL /IM RobloxPlayerBeta.exe /F",shell= True)
         #loops until biome found
-    # :zxczxczxc
     #print("ended")
 
 unbreakablehumanspirit()
+os.remove("tempscreen.png") #clear tempscreen upon closing
